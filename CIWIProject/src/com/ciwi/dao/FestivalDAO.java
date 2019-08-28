47d8a17998e4e7a24f0c3aeb6ae66e9fcd14af0d
sist192 <sist192@DESKTOP-ECANQ8L>
2019-08-28 16:16:54 +0900

    dao mappper 수정

--- CIWIProject/src/com/ciwi/dao/FestivalDAO.java
+++ CIWIProject/src/com/ciwi/dao/FestivalDAO.java
@@ -11,24 +11,31 @@ public class FestivalDAO {
 	static {
 		ssf = CreateSqlSessionFactory.getSsf();
 	}
-	
+
 	public static void festivalDataInsert(FestivalVO vo) {
 		SqlSession session = ssf.openSession(true); // getConnection()
 		session.insert("festivalDataInsert", vo);
 		session.close(); // disConnection()
 	}
-	
+
 	public static List<FestivalVO> festivalAllData() {
-		SqlSession session=ssf.openSession();
-		List<FestivalVO> list=session.selectList("festivalAllData");
+		SqlSession session = ssf.openSession();
+		List<FestivalVO> list = session.selectList("festivalAllData");
 		session.close();
 		return list;
 	}
-	
+
 	public static FestivalVO festivalDetail(int fno) {
-		SqlSession session=ssf.openSession();
-		FestivalVO vo=session.selectOne("festivalDetail", fno);
+		SqlSession session = ssf.openSession();
+		FestivalVO vo = session.selectOne("festivalDetail", fno);
 		session.close();
 		return vo;
+	}
+
+	public static List<FestivalVO> festivalSearch(Map map) {
+		SqlSession session = ssf.openSession();
+		List<FestivalVO> list = session.selectList("festivalSearch",map);
+		session.close();
+		return list;
 	}
 }
--- CIWIProject/src/com/ciwi/vo/FestivalVO.java
+++ CIWIProject/src/com/ciwi/vo/FestivalVO.java
@@ -13,89 +13,139 @@ public class FestivalVO {
 	private String tel;
 	private String showtime;
 	private int flike;
-	private int concert_genre;
+	private int festival_genre;
 	private int category_no;
 	private int area_no;
-	
-	
-	public int getConcert_genre() {
-		return concert_genre;
+	private FestivalGenreVO fgvo = new FestivalGenreVO();
+	private CIWICategoryVO ccvo = new CIWICategoryVO();
+	private AreaVO avo = new AreaVO();
+
+	public void setFestival_genre(int festival_genre) {
+		this.festival_genre = festival_genre;
 	}
-	public void setConcert_genre(int concert_genre) {
-		this.concert_genre = concert_genre;
+
+	public int getFestival_genre() {
+		return festival_genre;
 	}
+
+	public FestivalGenreVO getFgvo() {
+		return fgvo;
+	}
+
+	public void setFgvo(FestivalGenreVO fgvo) {
+		this.fgvo = fgvo;
+	}
+
+	public CIWICategoryVO getCcvo() {
+		return ccvo;
+	}
+
+	public void setCcvo(CIWICategoryVO ccvo) {
+		this.ccvo = ccvo;
+	}
+
+	public AreaVO getAvo() {
+		return avo;
+	}
+
+	public void setAvo(AreaVO avo) {
+		this.avo = avo;
+	}
+
 	public int getCategory_no() {
 		return category_no;
 	}
+
 	public void setCategory_no(int category_no) {
 		this.category_no = category_no;
 	}
+
 	public int getArea_no() {
 		return area_no;
 	}
+
 	public void setArea_no(int area_no) {
 		this.area_no = area_no;
 	}
+
 	public String getFdate() {
 		return fdate;
 	}
+
 	public void setFdate(String fdate) {
 		this.fdate = fdate;
 	}
+
 	public int getFlike() {
 		return flike;
 	}
+
 	public void setFlike(int flike) {
 		this.flike = flike;
 	}
+
 	public int getFno() {
 		return fno;
 	}
+
 	public void setFno(int fno) {
 		this.fno = fno;
 	}
+
 	public String getPlace() {
 		return place;
 	}
+
 	public void setPlace(String place) {
 		this.place = place;
 	}
+
 	public String getSubject() {
 		return subject;
 	}
+
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
+
 	public String getPoster() {
 		return poster;
 	}
+
 	public void setPoster(String poster) {
 		this.poster = poster;
 	}
+
 	public String getHost() {
 		return host;
 	}
+
 	public void setHost(String host) {
 		this.host = host;
 	}
+
 	public String getHomepage() {
 		return homepage;
 	}
+
 	public void setHomepage(String homepage) {
 		this.homepage = homepage;
 	}
+
 	public String getTel() {
 		return tel;
 	}
+
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
+
 	public String getShowtime() {
 		return showtime;
 	}
+
 	public void setShowtime(String showtime) {
 		this.showtime = showtime;
 	}
-	
-	
+
 }
