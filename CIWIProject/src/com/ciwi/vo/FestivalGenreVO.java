d32c3f7c1bd8ed174443de485b3c02dea5132275
sist192 <sist192@DESKTOP-ECANQ8L>
2019-08-28 16:23:34 +0900

    페스티벌 장르 VO 추가

---
+++ CIWIProject/src/com/ciwi/vo/FestivalGenreVO.java
@@ -0,0 +1,23 @@
+package com.ciwi.vo;
+
+public class FestivalGenreVO {
+	private int festival_no;
+	private String festival_genre;
+
+	public int getFestival_no() {
+		return festival_no;
+	}
+
+	public void setFestival_no(int festival_no) {
+		this.festival_no = festival_no;
+	}
+
+	public String getFestival_genre() {
+		return festival_genre;
+	}
+
+	public void setFestival_genre(String festival_genre) {
+		this.festival_genre = festival_genre;
+	}
+//袴走けいしけいし
+}
