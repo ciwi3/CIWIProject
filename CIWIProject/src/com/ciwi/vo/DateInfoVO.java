ce071ca2eba2f21b8ca49163079033d331b52e25
sist183 <sist183@DESKTOP-1312FRU>
2019-09-02 10:04:35 +0900

    DateInfoVO 추가

---
+++ CIWIProject/src/com/ciwi/vo/DateInfoVO.java
@@ -0,0 +1,19 @@
+package com.ciwi.vo;
+
+public class DateInfoVO {
+	private int dno;
+	private String timeno; // ÂüÁ¶
+	public int getDno() {
+		return dno;
+	}
+	public void setDno(int dno) {
+		this.dno = dno;
+	}
+	public String getTimeno() {
+		return timeno;
+	}
+	public void setTimeno(String timeno) {
+		this.timeno = timeno;
+	}
+	
+}
