c72cbd30eecc5398f5064db323eeecc6348d2415
sist183 <sist183@DESKTOP-1312FRU>
2019-09-02 10:09:10 +0900

    TimeinfoVO 추가

---
+++ CIWIProject/src/com/ciwi/vo/TimeInfoVO.java
@@ -0,0 +1,18 @@
+package com.ciwi.vo;
+
+public class TimeInfoVO {
+	private int timeno;
+	private String time;
+	public int getTimeno() {
+		return timeno;
+	}
+	public void setTimeno(int timeno) {
+		this.timeno = timeno;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
