ec54002747f7aff02aebebe6f31a79a0d5898116
sist183 <sist183@DESKTOP-1312FRU>
2019-09-05 15:36:01 +0900

    190905 jjimVO추가, config.xml에 jjimVO추가, config.xml에 jjim-mapper.xml추가 mapper패키지에 jjim-mapper.xml추가

---
+++ CIWIProject/src/com/ciwi/vo/JjimVO.java
@@ -0,0 +1,26 @@
+package com.ciwi.vo;
+
+public class JjimVO {
+	private int category_no;
+	private int contents_no;
+	private String id;
+	
+	public int getCategory_no() {
+		return category_no;
+	}
+	public void setCategory_no(int category_no) {
+		this.category_no = category_no;
+	}
+	public int getContents_no() {
+		return contents_no;
+	}
+	public void setContents_no(int contents_no) {
+		this.contents_no = contents_no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
